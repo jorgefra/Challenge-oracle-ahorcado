9ba54d87d3ee986dc45374d97cf437009b5d2468
Jorge <jorge-f_a@outlook.com>
2022-06-04 00:37:21 -0400

    archivos js extras borrados, funciones para el canva creado

--- js/addPalabra.js
+++
@@ -1,5 +0,0 @@
-var boton_palabra = document.getElementsByClassName('boton-palabra')
-
-
-
-
--- js/funciones.js
+++ js/funciones.js
@@ -1,19 +1,52 @@
+var tablero = document.getElementById('canvas').getContext('2d');
+
+function dibujarLinea(){
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.strokeStyle = "#0A3871"
+    tablero.beginPath();
+
+    var ancho = 250/palabraSecreta.length;
+    for(let i = 0; i < palabraSecreta.length; i++){
+        tablero.moveTo(20+(ancho*i),80)
+        tablero.lineTo(40+(ancho*i),80)
+    }
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarLetra(index){
+    tablero.font = "bold 40px Arial"
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "black"
+
+    var ancho = 250/palabraSecreta.length;
+    tablero.fillText(palabraSecreta[index],18+(ancho*index),60)
+}
+
+function dibujarLetraIncorrecta(letra, intentos){
+    tablero.font = "bold 30px Arial"
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "gray"
+
+    tablero.fillText(letra,10+(10*(10-intentos)),200,40)
+}
 
 
 function cambiarPantalla(pantallaClase){
-    console.log('funcion')
     var pantalla_juego = document.querySelector('.pantalla-juego')
     var pantalla_inicio = document.querySelector('.pantalla-inicio')
     var pantalla_palabra = document.querySelector('.pantalla-palabra')
-    
     var pantalla = document.querySelector(pantallaClase)
-
     pantalla_inicio.style.display = "none";
     pantalla_juego.style.display = "none";
     pantalla_palabra.style.display = "none";
-
     pantalla.style.display = "flex";
-
 }
 
 function agregarPalabra(){
@@ -27,10 +60,11 @@ function agregarPalabra(){
     return true;
 }
 
-function randomPalabra(min, max) {
-    return palabras[Math.floor((Math.random() * (max - min + 1)) + min)];
+function randomPalabra() {
+    let max = palabras.length-1;
+    console.log(palabras)
+    return palabras[Math.floor((Math.random() * (max + 1)))];
 }
-
 
 function capturarLetra(event) {
     var codigo = event.which || event.keyCode;
@@ -39,3 +73,4 @@ function capturarLetra(event) {
       return String.fromCharCode(codigo);
     }
 }
+
